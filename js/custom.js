532ce8a7b4f4d0ad05b0d156d4a98c3933b22e76
arayer143 <89434892+arayer143@users.noreply.github.com>
2023-08-18 00:08:42 -0500

    Removal of a lot more unused files

--- js/custom.js
+++ js/custom.js
@@ -42,18 +42,7 @@ jQuery(document).ready(function($) {
 		// tooltip
 		$('.social-network li a, .options_box .color a').tooltip();
 
-		// fancybox
-		$(".fancybox").fancybox({				
-				padding : 0,
-				autoResize: true,
-				beforeShow: function () {
-					this.title = $(this.element).attr('title');
-					this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
-				},
-				helpers : {
-					title : { type: 'inside' },
-				}
-			});
+	
 
 		
 		//scroll to top

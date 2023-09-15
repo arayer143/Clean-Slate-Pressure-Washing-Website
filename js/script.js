e00fcea2c8fc3fce2c01bfc7b52fcd347e47add2
arayer143 <89434892+arayer143@users.noreply.github.com>
2023-09-15 13:50:55 -0500

    addition of new review slider section

--- js/script.js
+++ js/script.js
@@ -4,7 +4,33 @@
 
 
 
-
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {  
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
 	
 
 
